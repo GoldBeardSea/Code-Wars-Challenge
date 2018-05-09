c23d2130fe5fdb7a362af4c6961bdcaf4cf122d5
GoldbeardSea <timbusch1@gmail.com>
2018-05-08 21:39:09 -0700

    solution

---
+++ my-head-is-on-the-wrong-end/soultion.js
@@ -0,0 +1,9 @@
+function fixTheMeerkat(arr) {
+  //your code here
+  let arrNew = [];
+  arrNew.push(arr[2]);
+  arrNew.push(arr[1]);
+  arrNew.push(arr[0]);
+  return arrNew;
+  //take that reverse method I can make more work than I need to if I want.
+}
