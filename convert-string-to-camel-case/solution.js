d0b2ab68ed23c700006bc144c0cff9ff6d461127
GoldbeardSea <timbusch1@gmail.com>
2018-05-26 16:53:38 -0700

    did this change the code?

--- convert-string-to-camel-case/solution.js
+++ convert-string-to-camel-case/solution.js
@@ -6,11 +6,10 @@ function toCamelCase(str){
     let i = 1;
     while (i < strArray.length) {
       strArray[i] = strArray[i][0].toUpperCase() + strArray[i].slice(1);
-      //  strArray[2] = strArray[2][0].toUpperCase() + strArray[2].slice(1);
       console.log(strArray);
       i++;
     }
-    strArray.join('');
+    strArray = strArray.join('');
     return strArray;
   }
 }
